9bd0fdd4aec3e4237bbdb0429d91a81fd87a0d53
tataaval <94060509+tataaval@users.noreply.github.com>
2026-01-02 15:22:56 +0400

    [ADD]: project folder  and gitignore

---
+++ Accessa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Accessa
+//
+//  Created by Tatarella on 02.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
